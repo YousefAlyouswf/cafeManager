f8093ae5fac8f13c88a3044261efd99f37f54375
yousefalyouswf﻿ <api.chc1989@gmail.com>
2020-03-02 21:57:37 -0600

    Add seat done

--- lib/main_screen/manager_services/cafe_services/add_seats.dart
+++ lib/main_screen/manager_services/cafe_services/add_seats.dart
@@ -11,45 +11,94 @@ class AddSeats extends StatefulWidget {
 
 class _AddSeatsState extends State<AddSeats> {
   String seatNum;
+  String msgErr;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("إدارة مقهى ${widget.cafeName}"),
+        title: Text("أظافة جلسات"),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Text("Add seats"),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            onChanged: (val) {
-              setState(() {
-                seatNum = val;
-              });
-            },
-          ),
-          RaisedButton(
-            onPressed: () {
-              Firestore.instance
-                  .collection('seats')
-                  .document(widget.cafeName)
-                  .updateData({
-                'allseats': FieldValue.arrayUnion([
-                  {
-                    'seat': seatNum,
-                    'color': 'green',
-                    'userid': '',
-                    'username': '',
-                    'userphone': '',
-                    'time': '',
-                  }
-                ]),
-              });
-            },
-            child: Text("A D D"),
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              keyboardType: TextInputType.number,
+              textAlign: TextAlign.end,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'رقم الجلسة',
+                hintStyle: TextStyle(color: Colors.grey, fontFamily: 'topaz'),
+              ),
+              onChanged: (val) {
+                setState(() {
+                  seatNum = val;
+                });
+              },
+            ),
+            Builder(
+              builder: (BuildContext context) {
+                return RaisedButton(
+                  onPressed: () async {
+                    if (seatNum == null || seatNum == '') {
+                      msgErr = 'أكتب رقم الجلسة';
+                    } else {
+                      msgErr = null;
+
+                      await Firestore.instance
+                          .collection('seats')
+                          .document(widget.cafeName)
+                          .updateData({
+                        'allseats': FieldValue.arrayUnion([
+                          {
+                            'seat': seatNum,
+                            'color': 'green',
+                            'userid': '',
+                            'username': '',
+                            'userphone': '',
+                            'time': '',
+                          }
+                        ]),
+                      });
+                      SnackBar mySnackBar = SnackBar(
+                        content: Text(
+                          "تم بنجاح",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        backgroundColor: Colors.green,
+                        duration: const Duration(milliseconds: 500),
+                      );
+                      Scaffold.of(context).showSnackBar(mySnackBar);
+                    }
+
+                    if (msgErr != null) {
+                      SnackBar mySnackBar = SnackBar(
+                        content: Text(
+                          msgErr,
+                          textAlign: TextAlign.end,
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        backgroundColor: Colors.red,
+                        duration: const Duration(milliseconds: 500),
+                      );
+                      Scaffold.of(context).showSnackBar(mySnackBar);
+                    }
+                  },
+                  child: Text(
+                    "إظافة",
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                  ),
+                  color: Colors.pink[400],
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
